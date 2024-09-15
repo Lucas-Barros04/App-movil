1eca8a47c0ff0494616c826a7b64086ed0392d2b
joshuacua <josh.cortesa@duocuc.cl>
2024-09-15 16:18:03 -0300

    listo-0.3

--- src/app/component/buscar/tarjetas-nombre/tarjetas-nombre.component.ts
+++ src/app/component/buscar/tarjetas-nombre/tarjetas-nombre.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input ,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tarjetas-nombre',
@@ -6,51 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tarjetas-nombre.component.scss'],
 })
 export class TarjetasNombreComponent  implements OnInit {
-buscar = [
-    {
-      imagen: { url: 'https://ionicframework.com/docs/img/demos/card-media.png' },
-      titulo: 'Card Title 1',
-      subtitulo: 'Card Subtitle 1',
-      descripcion: 'Descripción de la tarjeta 1.'
-    },
-    {
-      imagen: { url: 'https://ionicframework.com/docs/img/demos/card-media.png' },
-      titulo: 'Card Title 2',
-      subtitulo: 'Card Subtitle 2',
-      descripcion: 'Descripción de la tarjeta 2.'
-    },
-    {
-      imagen: { url: 'https://ionicframework.com/docs/img/demos/card-media.png' },
-      titulo: 'Card Title 3',
-      subtitulo: 'Card Subtitle 3',
-      descripcion: 'Descripción de la tarjeta 3.'
-    },
-    {
-      imagen: { url: 'https://ionicframework.com/docs/img/demos/card-media.png' },
-      titulo: 'Card Title 4',
-      subtitulo: 'Card Subtitle 4',
-      descripcion: 'Descripción de la tarjeta 4.'
-    },
-    {
-      imagen: { url: 'https://ionicframework.com/docs/img/demos/card-media.png' },
-      titulo: 'Card Title 5',
-      subtitulo: 'Card Subtitle 5',
-      descripcion: 'Descripción de la tarjeta 5.'
-    },
-    {
-      imagen: { url: 'https://ionicframework.com/docs/img/demos/card-media.png' },
-      titulo: 'Card Title 6',
-      subtitulo: 'Card Subtitle 6',
-      descripcion: 'Descripción de la tarjeta 6.'
-    },
-    {
-      imagen: { url: 'https://ionicframework.com/docs/img/demos/card-media.png' },
-      titulo: 'Card Title 7',
-      subtitulo: 'Card Subtitle 7',
-      descripcion: 'Descripción de la tarjeta 7.'
-    },
-  ];
-  searchText: string = '';
+  @Input() buscar: { imagen: { url: string }, titulo: string, subtitulo: string, descripcion: string }[] = []; 
+  @Input() searchText: string = ''; // Recibe el texto de búsqueda
 
   get filteredCards() {
     return this.buscar.filter(tarjeta =>
